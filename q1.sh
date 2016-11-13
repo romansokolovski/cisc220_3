1f09814e4c75b767e6ae30a8025a1e0c6174ae6d
fang <xufang5555@hotmail.com>
2016-11-13 18:50:44 -0500

    cal of sum finished

--- q1.sh
+++ q1.sh
@@ -7,14 +7,19 @@
 
 
 
-function main{
-	for (( i=1000; i<=10000; i++ )); do 
-		numString="$i"
-#		sum=0
-#		sum=$(( $sum+($num%10) ))
 
-		echo $numString
+function main {
+	for (( i=1000; i<=1010; i++ )); do 
+		numb=$i
+		sum=0
+		echo the number is: $numb
+		while [[ $numb -gt 0 ]] ; do 
+			x=`expr $numb % 10`
+			sum=`expr $sum + $x`
+			numb=`expr $numb / 10`
+		done
+		echo the sum of $i is $sum
 	done
 }
 
-main()
+main
